898efcc540bbd241523a65caeafedcb894bd2d30
Gonzalo <undying.gate@gmail.com>
2018-12-04 18:19:42 -0300

    'arreglo'

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,8 +13,8 @@ public class Greeter {
   }
 
   /**
-  *@param someone nombre persona
-  *@return greeting string
+  * @param someone nombre persona
+  * @return greeting string
   */  
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
